af5e42aae7d7ab2f8aa12a571f3788d835c9ed13
Afeef K K <45afeef@gmail.com>
2022-10-14 22:28:53 +0530

    Added CircleTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CircleTask from './components/CircleTask';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <CircleTask  name="Vedio Hub" desc='To share media between business and its users' tasks={[]}/>
       </header>
     </div>
   );
---
+++ src/components/CircleTask/CircleTask.tsx
@@ -0,0 +1,42 @@
+import { FC, useState } from "react";
+import s from "./CircleTask.module.css";
+
+type task = {
+  name: string;
+  desc: string;
+  tasks: task[];
+};
+
+const CircleTask: FC<task> = ({ name, desc, tasks }) => {
+  // Utility fucntion to refresh the component
+  const [state, setState] = useState(true);
+  const refresh = () => setState(!state);
+
+  const initNewTask = () => {
+    tasks.push({ name: "1", desc: "3", tasks: [] });
+    refresh();
+  };
+
+  console.log("size of task " + tasks.length);
+  return (
+    <div className={s.root}>
+      <h1 className={s.heading}>{name}</h1>
+      <p className={s.desc}>{desc}</p>
+      {tasks.length < 5 && (
+        <img className={s.button} src="./add.svg" onClick={initNewTask} />
+      )}
+      {tasks.map((task, i) => {
+        return (
+          <div key={i} className={s.orbit}>
+            <div className={s.task}>
+              <h1>Task {i}</h1>
+              <p>{task.name}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CircleTask;
