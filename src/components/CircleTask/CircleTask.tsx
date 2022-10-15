c0f23beeee02d97398dc163f8b4636b5bbb6e74d
Afeef K K <45afeef@gmail.com>
2022-10-15 19:02:48 +0530

    Better Task selection with hover animation

--- src/components/CircleTask/CircleTask.tsx
+++ src/components/CircleTask/CircleTask.tsx
@@ -12,6 +12,8 @@ const CircleTask: FC<task> = ({ name, desc, tasks }) => {
   const [state, setState] = useState(true);
   const refresh = () => setState(!state);
 
+  const [selected, setSelected] = useState(-1);
+
   const initNewTask = () => {
     tasks.push({ name: "Task Name", desc: "description", tasks: [] });
     refresh();
@@ -19,27 +21,53 @@ const CircleTask: FC<task> = ({ name, desc, tasks }) => {
 
   const degreeOfSection = 360 / (tasks.length + 1);
   return (
-    <div className={s.root}>
+    <div className={s.root} onMouseLeave={() => setSelected(-1)}>
       <h1 className={s.heading}>{name}</h1>
       <p className={s.desc}>{desc}</p>
-      {tasks.length < 10 && (
+      {tasks.length < 11 && (
         <img className={s.button} src="./add.svg" onClick={initNewTask} />
       )}
-      {tasks.map((task, i) => {
+      {tasks.map(({ name, desc }, i) => {
         const rotation = degreeOfSection * (i + 1);
         return (
-          <div key={i} className={s.axis} style={{ rotate: `-${rotation}deg` }}>
+          <div
+            key={i}
+            className={s.axis}
+            style={{
+              rotate: `-${rotation}deg`,
+              height:
+                selected === -1 ? "90%" : selected === i ? "30vmin" : "105%",
+            }}
+          >
             <div>
-              <div className={s.task} style={{ rotate: `${rotation}deg` }}>
-                <div className={s.taskProgress}>
+              <div
+                className={s.task}
+                style={{
+                  rotate: `${rotation}deg`,
+                  scale: selected === -1 ? ".8" : selected === i ? "2" : ".3",
+                }}
+                onMouseEnter={() => setSelected(i)}
+              >
+                <div
+                  className={s.taskProgress}
+                  style={{ height: `${i * 10}%` }}
+                >
                   <img src="./waves.svg" />
                   <div></div>
                 </div>
-                <h1 contentEditable suppressContentEditableWarning>
-                  {task.name} {i}
+                <h1
+                  contentEditable
+                  suppressContentEditableWarning
+                  onFocus={() => setSelected(i)}
+                >
+                  {name} {i}
                 </h1>
-                <p contentEditable suppressContentEditableWarning>
-                  {task.desc}
+                <p
+                  contentEditable
+                  suppressContentEditableWarning
+                  onFocus={() => setSelected(i)}
+                >
+                  {desc}
                 </p>
               </div>
             </div>
